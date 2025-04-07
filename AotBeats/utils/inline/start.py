cb40c98cddf05911c2d0082c6d16a41385c18aea
Itachi <hritikraj123412312@gmail.com>
2025-04-08 02:01:06 +0530

    Update start.py

--- AotBeats/utils/inline/start.py
+++ AotBeats/utils/inline/start.py
@@ -19,16 +19,6 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-        [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
